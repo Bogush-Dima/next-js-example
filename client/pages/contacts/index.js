a6e2093a97517a0b555c5e189b67a126f7889d3f
Dzmitry-Bohush <dzmitry.bohush@innowise-group.com>
2022-07-20 16:31:34 +0300

    feat: added request for getting contacts

--- client/pages/contacts/index.js
+++ client/pages/contacts/index.js
@@ -1,8 +1,32 @@
 import { Heading } from "../components/Heading"
 
-const Contacts = () => {
+export const getStaticProps = async() => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await response.json()
+
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      contacts: data
+    }
+  }
+}
+
+const Contacts = ({ contacts }) => {
   return (
-    <Heading text="Contacts List" />
+    <>
+      <Heading text="Contacts List" />
+      {contacts.map(({id, name, email}) => (
+        <li key={id}>
+          <strong>{name}</strong> ({email})
+        </li>
+      ))}
+    </>
   )
 }
 
