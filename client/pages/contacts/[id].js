a244a1cf1cd92fdda7e2c896664f13beeafe832b
Dzmitry-Bohush <dzmitry.bohush@innowise-group.com>
2022-07-28 14:46:21 +0300

    feat: create SSR request

--- client/pages/contacts/[id].js
+++ client/pages/contacts/[id].js
@@ -1,8 +1,26 @@
 import { ContactInfo } from "../components/ContactInfo"
 
-const Contact = () => {
+export const getServerSideProps = async(context) => {
+  const { id } = context.params
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  const data = await response.json()
+
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      contact: data
+    }
+  }
+}
+
+const Contact = ({ contact }) => {
   return (
-    <ContactInfo />
+    <ContactInfo contact={contact} />
   )
 }
 
